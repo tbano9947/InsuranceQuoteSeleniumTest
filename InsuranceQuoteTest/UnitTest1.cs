12270558e388f6db7d5b175f2376f4a2f1a1fbcc
Tasnim Bano Tasnim Bano <Tbano9947@conestogac.on.ca>
2025-04-09 01:47:41 -0400

    added test cases

---
+++ InsuranceQuoteTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace InsuranceQuoteTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
